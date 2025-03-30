34ce2f0ddda63cfcab9205625fad9a7913748f24
Sun Rising <275050943@qq.com>
2025-03-30 18:28:59 +0800

    添加 构建升级包脚本

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -8,12 +8,9 @@ import moment from "moment";
 import {
   copyFileSync,
   existsSync,
-  mkdir,
   mkdirSync,
   readdirSync,
-  statSync,
   writeFileSync,
-  writeSync,
 } from "node:fs";
 import { exec } from "node:child_process";
 import { promisify } from "node:util";
@@ -60,34 +57,34 @@ async function run(option) {
       pubspec.version,
       [nextVersion, versionArr[1]].join("+")
     );
-    // writeFileSync(pubspecPath, pubspecStr, {
-    //   encoding: "utf8",
-    // });
+    writeFileSync(pubspecPath, pubspecStr, {
+      encoding: "utf8",
+    });
     console.log(`开始构建新版本：${nextVersion}，请稍等...`);
 
     //
-    // await execPromess(`flutter build apk --release`, {
-    //   cwd: join(__dirnameNew, "../"),
-    // });
+    await execPromess(`flutter build apk --release`, {
+      cwd: join(__dirnameNew, "../"),
+    });
     const filesName = readdirSync(flutterOutPutDir);
     const apkFileName = filesName.find((name) => name.includes(".apk"));
     const packageDir = join(outPutDir, `v${nextVersion}/`);
-    if (!existsSync(packageDir)) {
-      mkdirSync(packageDir);
+    if (existsSync(packageDir)) {
+      fs.removeSync(packageDir)
     }
+    mkdirSync(packageDir);
     copyFileSync(
       join(flutterOutPutDir, apkFileName),
       join(packageDir, apkFileName)
     );
     //
     const releaseData = {
-      lastVersion: pubspec.version,
+      lastVersion: versionArr[0],
       version: nextVersion,
       fileName: apkFileName,
       time: moment().format("YYYY-MM-DD"),
       upgradeInstructions: ["修复已知问题"],
     };
-    console.log(fs);
     fs.writeJSONSync(join(packageDir, releaseDataFileName), releaseData, {
       encoding: "utf8",
       spaces: 2,
